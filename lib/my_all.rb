d5b1967caba32c3d1bd0742ff470999577517356
Scott Silverman <scottms724@gmail.com>
2018-05-02 14:47:50 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,10 +4,10 @@ def my_all?(collection)
   i = 0 
   block_return_values = []
     while i < collection.length 
-    black_return_values << yield(collection[i])
+    block_return_values << yield(collection[i])
     i += 1 
     end
-    if black_return_values.include?(false)
+    if block_return_values.include?(false)
       false 
     else 
       true 
